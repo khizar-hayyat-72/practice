c70977830893ea32cffc8a29a70c119591d89cf2
khizar-hayyat-72 <Khizarhayyt199093@gmail.com>
2022-02-23 18:36:47 +0500

    State with State life cycle

--- src/Clock.js
+++ src/Clock.js
@@ -1,12 +1,33 @@
 import React from "react";
-class Clock extends React.Component{
-    render(){
-        return(
-            <div>
-                 {this.props.Date.toLocaleTimeString()}
-            </div>
-        ); 
-    }
-}
 
-export default Clock;
+class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+    componentDidMount(){
+        this.timmerID = setInterval(
+            () => this.tick(), 1000
+        )
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timmerID)
+    }
+    tick(){
+        this.setState({
+            date: new Date()
+        });
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }
+
+    export default Clock;
