8561f721ae2994c8c7f7fc96d43cdb65d5025d29
khizar-hayyat-72 <Khizarhayyt199093@gmail.com>
2022-02-23 16:00:27 +0500

    JSX Basics From React.orgs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// function formatUser(user){
+//   return user.firstName +' '+ user.lastName;
+// }
+// const user = {
+//   firstName: ' Khizar Hayyat Jafar',
+//   lastName: ' KHJ'
+// };
+
+// function validate(user){
+//   if(user){
+//     return user.firstName +' '+ user.lastName;
+//   }
+//   else{
+//     return <h1>Strange user</h1>;
+//   }
+// }
+// const element1 = <h1>Hi,{ validate(user)}</h1>;
+
+//Element Creation by JSX
+const element = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Hello World'
+)
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ 
+  element,
   document.getElementById('root')
 );
 
